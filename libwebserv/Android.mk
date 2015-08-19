e08d45c4408289a1f69cb54d0b4ae12830e30180
Daniel Erat <derat@google.com>
2015-08-19 07:57:47 -0600

    Fix libwebserv header dir export.
    Export the base directory so that include paths like
    <libwebserv/foo.h> can be used.
    
    Bug: 22828211
    Change-Id: I58a51655d84681433379e3f7ed6d7be0ee98f2a1

--- libwebserv/Android.mk
+++ libwebserv/Android.mk
@@ -21,7 +21,7 @@ LOCAL_PATH := $(my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libwebserv
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/..
 # TODO: Add dbus_bindings/org.chromium.WebServer.RequestHandler.dbus.xml once
 # code generation is working.
 LOCAL_SRC_FILES := \
